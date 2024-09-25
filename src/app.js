159a3fd0df25a9a71154adfd5d4276aaeee3bf09
Agustin Gomez <agustingomezdev@gmail.com>
2024-09-26 00:25:55 +0200

    fixing cors

--- src/app.js
+++ src/app.js
@@ -9,9 +9,7 @@ const mainRouter = require('./routes/index')
 
 
 const app = express();
-
 const PORT = process.env.PORT
-
 const httpServer = app.listen(PORT, () => {
     console.log('Server running on port: ' + PORT)
 })
@@ -19,15 +17,19 @@ const httpServer = app.listen(PORT, () => {
 config.mongoInstance()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
+app.use(mainRouter)
+
 app.use(cors({
-    origin: [process.env.LOCALHOST_CORS, 'https://www.anunciosuy.com', 'https://anunciosuy.com'],
-    credentials: true
-}))
-app.options('*', cors({
-    origin: [process.env.LOCALHOST_CORS, 'https://www.anunciosuy.com', 'https://anunciosuy.com'],
+    origin: ['https://www.anunciosuy.com', 'https://anunciosuy.com', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true
 }));
-
-app.use(cookieParser());
-
-app.use(mainRouter)
+app.options('*', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(204);
+});
