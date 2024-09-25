6e173a8a7166bd27097a7daef745c60189f64501
Agustin Gomez <agustingomezdev@gmail.com>
2024-09-26 00:33:33 +0200

    trying to fix cors

--- src/app.js
+++ src/app.js
@@ -9,21 +9,13 @@ const mainRouter = require('./routes/index')
 
 
 const app = express();
-const PORT = process.env.PORT
-const httpServer = app.listen(PORT, () => {
-    console.log('Server running on port: ' + PORT)
-})
-
+const PORT = process.env.PORT || 3000
 config.mongoInstance()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser());
-app.use(mainRouter)
 
 app.use(cors({
     origin: ['https://www.anunciosuy.com', 'https://anunciosuy.com', 'http://localhost:5173'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 app.options('*', (req, res) => {
@@ -33,3 +25,12 @@ app.options('*', (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.sendStatus(204);
 });
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
+app.use(mainRouter)
+
+const httpServer = app.listen(PORT, () => {
+    console.log('Server running on port: ' + PORT)
+})
